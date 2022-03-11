23790a40be8ee9d530bcf1567cd55ec85d3f6d60
Ole Schoenherr <ole.schoenherr@haw-hamburg.de>
2022-03-11 23:02:17 +0100

    First PoC

--- Arduino/b-328-rosserial/b-328-rosserial.ino
+++ Arduino/b-328-rosserial/b-328-rosserial.ino
@@ -1,4 +1,5 @@
 #include <ros.h>
+#include <std_msgs/Int32.h>
 #include <std_msgs/Float32.h>
 
 #include <PIDController.h>
@@ -7,15 +8,13 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define F_CPU 20000000UL
-
 ros::NodeHandle nh;
 
 // Message definition
 std_msgs::Float32 encMsg;
 
 // Variables
-const uint8_t cpu_freq = 20; //MHz
+// const uint8_t cpu_freq = 20; //MHz
 
 typedef struct
 {
@@ -32,13 +31,13 @@ typedef struct
 
 MVS_t mvs[3];
 uint32_t ticksLastPID;
-int16_t curr_enc;
+int32_t curr_enc;
 int32_t last_enc;
-float getRPM;
+float getRPM = 0.;
 float setRPM;
 float getPWM;
 float getPIDVal;
-float lpf_factor;
+float lpf_factor=.1;
 float rpm_lpf;
 float pwm_lpf;
 volatile uint32_t ticksMostSig;
@@ -49,7 +48,7 @@ int8_t mvsBalance1;
 
 volatile uint8_t cache, cacheOld;
 
-PID_t PID = {0.000,
+PID_t PID = {0.100,
              5.000,
              0.000};
 
@@ -70,6 +69,7 @@ ros::Subscriber<std_msgs::Float32> sub("setSpeed", setSpeedCB);
 
 void setup() {
     controllerSetup();
+    nh.getHardware()->setBaud(93600);
     nh.initNode();
     nh.subscribe(sub);
     nh.advertise(pub);
@@ -81,10 +81,6 @@ void loop() {
 
     // Publisher stuff here:
 
-    encMsg.data = getRPM;
-    pub.publish(&encMsg);
-    
-
     // Handle the ROS stuff
     nh.spinOnce();
 }
@@ -92,12 +88,10 @@ void loop() {
 void controllerSetup()
 {
   /* SETUP */
-  Serial.begin(115200);
-
   pidCon.resetController();
 
   /// Pin - setup: PC 0-3 to input (PCINT8 - 11)
-  DDRC &= 0b11110000; // 0 => input / 1 => leave state
+  DDRC &= 0b11110011; // 0 => input / 1 => leave state
 
   /// Pin - setup: PD3, PD5, PD6, PB3 as output
   DDRD |= 0b01101001;
@@ -105,44 +99,52 @@ void controllerSetup()
 
   /// Timer0 / 2 [PWM-Setup]
   TCCR0A = TCCR2A = 0xF3; // Fast-PWM (inverting mode)
+/*
   TCCR0B = TCCR2B = 0x01; // start Timer prescaler clk/1 => F_CPU/255
+*/
   OCR0A = OCR0B = OCR2A = OCR2B = 0;
-
   /// EXTERNAL INTERUPT
   PCICR |= (1 << PCIE1);    // Enable Pin Change Interrupt
-  PCMSK1 = 0b00001111;      // Enable Pins PCINT8...PCINT11 (PC0...PC3) for Pin Change Interrupts
-
+  PCMSK1 = 0b00001100;      // Enable Pins PCINT8...PCINT11 (PC0...PC3) for Pin Change Interrupts
   /// Timer1 [Clock-Setup]
   OCR1A = 0xFFFF;           // Set Output Compare Register A of Timer 1 to 0xFFFF = 65536 = 16 bit
   TCCR1B = (1 << WGM12);    // Mode 4: Clear Timer on Compare Match (CTC): if(TCNT1==OCR1A){TCNT=0;}
   TIMSK1 |= (1 << OCIE1A);  // Enable Interrupt on Output Compare Match 
   TCCR1B |= (1 << CS10);    // Set Prescaler to 1 and start the timer 
+/*
                             // [original comment: set prescaler to 8 and start the timer (OSC)]
-
-  sei();                    // Enable Interrupts
-  //wdt_enable(WDTO_500MS);
-  wdt_enable(WDTO_15MS);    // Enable Watch Dog Timer with Time Out = 15 ms
   /* END SETUP */
+  //wdt_enable(WDTO_500MS);
+  // wdt_enable(WDTO_15MS);    // Enable Watch Dog Timer with Time Out = 15 ms
+  sei();                    // Enable Interrupts
 }
 
 void controllerLoop()
 {
-    wdt_reset(); // Reset Watchdog Timer
-    uint16_t ticks = getTicks(); // Read current value of TCNT1 
+    // wdt_reset(); // Reset Watchdog Timer
+    uint16_t ticks = micros(); // Read current value of TCNT1 
     uint16_t dtTick = ticks - ticksLastPID;
-    if (dtTick >= cpu_freq * 1000) // ~0.001s => 1kHz control-loop  ///TODO: overflow?!?!?!?
+    if (dtTick >= 4000) // ~0.001s => 1kHz control-loop  ///TODO: overflow?!?!?!?
     {
-        float dt = (dtTick) / (1000000.0f * cpu_freq);
+        float dt = (dtTick) / (1000000.0f);
         ticksLastPID = ticks;
 
         // calc vals
+        curr_enc = EncoderPos;
         int16_t d_enc = (curr_enc - last_enc);
         last_enc = curr_enc;
 
-        getRPM = (float)d_enc / dt;
+        getRPM = ((float)d_enc) / dt;
 
-        float pidVal = pidCon.runController(getRPM, setRPM, dt);
-        float pwm = pidVal + vorsteuerung(setRPM, mvsBalance0, mvsBalance1);
+        // calc lpf-vals
+        rpm_lpf += (getRPM - rpm_lpf) * lpf_factor;
+        pwm_lpf += (getPWM - pwm_lpf) * lpf_factor;
+
+        encMsg.data = rpm_lpf;
+        pub.publish(&encMsg);
+
+        float pidVal = pidCon.runController(rpm_lpf, setRPM, dt);
+        float pwm = pidVal; // + vorsteuerung(setRPM, mvsBalance0, mvsBalance1);
 
         getPIDVal += (pidVal - getPIDVal) * lpf_factor;  //debug
 
@@ -163,10 +165,6 @@ void controllerLoop()
         getPWM = pwm;
         
         setMotor(getPWM, 1);
-
-        // calc lpf-vals
-        rpm_lpf += (getRPM - rpm_lpf) * lpf_factor;
-        pwm_lpf += (getPWM - pwm_lpf) * lpf_factor;
 
         /*
         Serial.print("PWM:");
@@ -204,9 +202,9 @@ ISR(PCINT1_vect)
     uint8_t x1 = (cache ^ (cacheOld >> 1)); // Bitshift cacheOld to the right and XOR with cache
     uint8_t x2 = (cacheOld ^ (cache >> 1)); // Bitshift cache to the right and XOR with cacheOld
 
+    /*
     // ENCODER A (PC0 and PC1)
 
-    /*
 
     // && = Logical AND   & = Bitwise AND
     //      0 &&   0 = 0      0 & 0 = 0
@@ -215,14 +213,14 @@ ISR(PCINT1_vect)
     //    >=1 && >=1 = 1      1 & 1 = 1
     if ((x1 & 0b00000001) && !(x2 & 0b00000001))
     {
-    currRegs.EncoderPos--;
+    EncoderPos--;
     }
     if (!(x1 & 0b00000001) && (x2 & 0b00000001))
     {
-    currRegs.EncoderPos++;
+    EncoderPos++;
     }
-
     */
+
 
     // ENCODER B (PC2 and PC3)
     if ((x1 & 0b00000100) && !(x2 & 0b00000100))
